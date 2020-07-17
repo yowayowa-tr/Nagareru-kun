f58f8cd59d9e727a21d983c5a301f5a23e19002a
Yukibara <nitic.mtbr@gmail.com>
2020-07-17 10:43:41 +0900

    ✨コメントが流れるようになった
    なおバグ

--- src/MrNagareru/MrNagareru/MainWindow.xaml.cs
+++ src/MrNagareru/MrNagareru/MainWindow.xaml.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,9 +9,12 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Effects;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace MrNagareru
 {
@@ -34,7 +37,6 @@ namespace MrNagareru
             // 最前面に表示する
             this.Topmost = true;
         }
-
 
         private void textBox_TextChanged(object sender, TextChangedEventArgs e)
         {
@@ -78,9 +80,60 @@ namespace MrNagareru
         {
             // 代入
             string textValue = textBox.Text;
-            
+
+            // てすと あとでけす
+            CreateTextBox(textValue);
+
             // TODO: ここで送信を処理する関数にtextValueを渡したい
         }
 
+        // テキストボックスを作る
+        private void CreateTextBox(String comm)
+        {
+            Dispatcher.Invoke(new Action(() =>
+            {
+                var comment = new TextBlock
+                {
+                    FontSize = 25,
+                    TextWrapping = TextWrapping.NoWrap,
+                    Opacity = 0,
+                    Text = comm,
+                    Foreground = new SolidColorBrush(Colors.White),
+                    Background = new SolidColorBrush(Color.FromArgb(1, 0, 0, 0))
+                };
+
+                Root.Children.Add(comment);
+                Dispatcher.BeginInvoke(new Action(() =>
+                  {
+                      MoveComment(comment);
+                      comment.Opacity = 100;
+                  }), DispatcherPriority.Loaded);
+            }));
+        }
+
+        private void MoveComment(TextBlock comment)
+        {
+            var random = new Random();
+            Canvas.SetTop(comment, random.Next((int)comment.ActualHeight, (int)(Height * 0.9)));
+            var moveTime = random.Next(10000, 13000);
+            var animation = new DoubleAnimation
+            {
+                From = Width,
+                To = (-1) * comment.ActualWidth - 10,
+                Duration=TimeSpan.FromMilliseconds(moveTime)
+            };
+            var delet = new DispatcherTimer(DispatcherPriority.SystemIdle)
+            {
+                Interval=TimeSpan.FromMilliseconds(moveTime)
+            };
+            delet.Tick += (x,y) =>
+            {
+                delet.Stop();
+                Root.Children.Remove(comment);
+            };
+
+            comment.BeginAnimation(LeftProperty, animation);
+            delet.Start();
+        }
     }
 }
