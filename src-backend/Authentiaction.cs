72986b4a83e3c7edc2c7c580392e4a8937ede2f7
Rei Takahashi <usingfree1012@gmail.com>
2020-07-17 10:39:51 +0900

    init

---
+++ src-backend/Authentiaction.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Graph;
+using Microsoft.Graph.Auth;
+using Microsoft.Identity.Client;
+
+namespace src_backend
+{
+    class Authentiaction
+    {
+        private static string ClientId = "29ce940f-4c9f-4e54-a8f9-74b981a14475";
+        string[] scopes = new string[] { "user.read" };
+        string graphAPIEndpoint = "https://graph.microsoft.com/v1.0/me";
+
+        public async Task<GraphServiceClient> GetClient()
+        {
+            var app = PublicClientApplicationBuilder.Create(ClientId)
+                    .WithRedirectUri("https://login.microsoftonline.com/common/oauth2/nativeclient")
+                    .Build();
+
+            var accounts = await app.GetAccountsAsync();
+            AuthenticationResult authResult;
+
+            try
+            {
+                authResult = await app.AcquireTokenSilent(scopes, accounts.FirstOrDefault())
+                    .ExecuteAsync();
+            }
+            catch (MsalUiRequiredException)
+            {
+                authResult = await app.AcquireTokenInteractive(scopes)
+                                .ExecuteAsync();
+            }
+
+            string a = await GetHttpContentWithToken(graphAPIEndpoint, authResult.AccessToken);
+
+            DeviceCodeProvider authProvider = new DeviceCodeProvider(app, scopes);
+            return new GraphServiceClient(authProvider);
+        }
+
+        private async Task<string> GetHttpContentWithToken(string url, string token)
+        {
+            var httpClient = new System.Net.Http.HttpClient();
+            System.Net.Http.HttpResponseMessage response;
+            try
+            {
+                var request = new System.Net.Http.HttpRequestMessage(System.Net.Http.HttpMethod.Get, url);
+                request.Headers.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", token);
+                response = await httpClient.SendAsync(request);
+                var content = await response.Content.ReadAsStringAsync();
+                return content;
+            }
+            catch (Exception ex)
+            {
+                return ex.ToString();
+            }
+        }
+    }
+}
---
+++ src-backend/MainWindow.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using Microsoft.Identity.Client;
+
+namespace src_backend
+{
+
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+    }
+}
